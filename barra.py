5e9258d44e6df46e01bf6d3c7b00ae61db952596
MatiasLeon1 <43649125+MatiasLeon1@users.noreply.github.com>
2020-10-01 13:40:01 -0300

    Codigo barra Entrega 2

--- barra.py
+++ barra.py
@@ -27,9 +27,11 @@ class Barra(object):
         """Devuelve el largo de la barra. 
         ret: instancia de objeto tipo reticulado
         """
-        xi = reticulado.obtener_coordenada_nodal(self.ni)
-        xj = reticulado.obtener_coordenada_nodal(self.nj)
-        dij = xi-xj
+        x_i = reticulado.obtener_coordenada_nodal(self.ni)
+        x_j = reticulado.obtener_coordenada_nodal(self.nj)
+        
+        dij = x_i-x_j
+        
         return np.sqrt(np.dot(dij,dij))
 
     def calcular_peso(self, reticulado):
@@ -42,21 +44,32 @@ class Barra(object):
 
 
 
-
-
-
-
-
-
-
-
     def obtener_rigidez(self, ret):
         """Devuelve la rigidez ke del elemento. Arreglo numpy de (4x4)
         ret: instancia de objeto tipo reticulado
         """
         
         #implementar
+        xi=ret.xyz[self.ni,0]
+        yi=ret.xyz[self.ni,1]
+        
+        xj=ret.xyz[self.nj,0]
+        yj=ret.xyz[self.nj,1]
+        
+        menos_cos_teta = (xi-xj)/self.calcular_largo(ret)
+        menos_sin_teta = (yi-yj)/self.calcular_largo(ret)
+        mas_cos_teta   = (xj-xi)/self.calcular_largo(ret)
+        mas_sin_teta   = (yj-yi)/self.calcular_largo(ret)
 
+        T_teta= np.array([[menos_cos_teta], [menos_sin_teta], [mas_cos_teta], [mas_sin_teta]])
+
+        
+        A=self.calcular_area()
+        E_barra = self.E
+        L=self.calcular_largo(ret)
+        
+        ke = T_teta @ T_teta.T * (A*E_barra)/L
+        
         return ke
 
     def obtener_vector_de_cargas(self, ret):
@@ -65,7 +78,9 @@ class Barra(object):
         """
 
         #Implementar
-
+        W_partido_2=self.calcular_peso(ret)/2.0
+        fe=np.array([[0],[-1],[0],[-1]])*W_partido_2 #[[],[]]
+          
         return fe
 
 
@@ -75,7 +90,29 @@ class Barra(object):
         """
 
         #Implementar
+        A=self.calcular_area()
+        E_barra = self.E
+        L=self.calcular_largo(ret)
+        
+        nodo1=self.ni
+        nodo2=self.nj
+        ue=np.array([ret.u[nodo1*2], ret.u[((2*nodo1)+1)], ret.u[2*nodo2], ret.u[((2*nodo2)+1)]])
+        
+        xi=ret.xyz[self.ni,0]
+        yi=ret.xyz[self.ni,1]
+        
+        xj=ret.xyz[self.nj,0]
+        yj=ret.xyz[self.nj,1]
+        
+        menos_cos_teta = (xi-xj)/L
+        menos_sin_teta = (yi-yj)/L
+        mas_cos_teta   = (xj-xi)/L
+        mas_sin_teta   = (yj-yi)/L
 
+        T_teta= np.array([[menos_cos_teta], [menos_sin_teta], [mas_cos_teta], [mas_sin_teta]])
+        
+        
+        se = (A*E_barra)/L * T_teta.T @ ue
 
         return se
 
