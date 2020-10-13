2bd439852bed8f3483a51d53199f8b831d216ad8
MatiasLeon1 <43649125+MatiasLeon1@users.noreply.github.com>
2020-10-13 18:00:33 -0300

    Update Barra

--- barra.py
+++ barra.py
@@ -48,7 +48,6 @@ class Barra(object):
         return self.ρ * A * L * g
 
 
-
     def obtener_rigidez(self, ret):
         A = self.calcular_area()
         L = self.calcular_largo(ret)
@@ -56,23 +55,23 @@ class Barra(object):
         xi = ret.obtener_coordenada_nodal(self.ni)
         xj = ret.obtener_coordenada_nodal(self.nj)
 
-        cosθ = (xj[0] - xi[0])/L
-        sinθ = (xj[1] - xi[1])/L
+        cosθx = (xj[0] - xi[0])/L
+        cosθy = (xj[1] - xi[1])/L
+        cosθz = (xj[2] - xi[2])/L
 
-        Tθ = np.array([ -cosθ, -sinθ, cosθ, sinθ ]).reshape((4,1))
+        Tθ = np.array([ -cosθx, -cosθy,-cosθz, cosθx, cosθy, cosθz ]).reshape((6,1))
 
         return self.E * A / L * (Tθ @ Tθ.T )
 
     def obtener_vector_de_cargas(self, ret):
         W = self.calcular_peso(ret)
-
-        return np.array([0, -W, 0, -W])
+        return np.array([0,0, -W, 0,0, -W])
 
 
     def obtener_fuerza(self, ret):
-        ue = np.zeros(4)
-        ue[0:2] = ret.obtener_desplazamiento_nodal(self.ni)
-        ue[2:] = ret.obtener_desplazamiento_nodal(self.nj)
+        ue = np.zeros(6)
+        ue[0:3] = ret.obtener_desplazamiento_nodal(self.ni)
+        ue[3:] = ret.obtener_desplazamiento_nodal(self.nj)
         
         A = self.calcular_area()
         L = self.calcular_largo(ret)
@@ -80,16 +79,19 @@ class Barra(object):
         xi = ret.obtener_coordenada_nodal(self.ni)
         xj = ret.obtener_coordenada_nodal(self.nj)
 
-        cosθ = (xj[0] - xi[0])/L
-        sinθ = (xj[1] - xi[1])/L
+        cosθx = (xj[0] - xi[0])/L
+        cosθy = (xj[1] - xi[1])/L
+        cosθz = (xj[2] - xi[2])/L
 
-        Tθ = np.array([ -cosθ, -sinθ, cosθ, sinθ ]).reshape((4,1))
+        Tθ = np.array([ -cosθx, -cosθy,-cosθz, cosθx, cosθy, cosθz ]).reshape((6,1))
 
         return self.E * A / L * (Tθ.T @ ue)
 
 
 
-    def chequear_diseño(self, Fu, ϕ=0.9):
+
+
+    def chequear_diseño(self, Fu,ret, ϕ=0.9):
         """Para la fuerza Fu (proveniente de una combinacion de cargas)
         revisar si esta barra cumple las disposiciones de diseño.
         """
@@ -100,7 +102,7 @@ class Barra(object):
             return False
         else:
             return True
-
+        
 
     def obtener_factor_utilizacion(self, Fu, ϕ=0.9):
         """Para la fuerza Fu (proveniente de una combinacion de cargas)
